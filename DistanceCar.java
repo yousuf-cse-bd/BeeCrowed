be466e8e8097e498e2f157b4595649c2447a6621
yousuf-cse-bd <yousuf.cse17@gmail.com>
2025-03-14 09:22:17 +0600

    1016

---
+++ DistanceCar.java
@@ -0,0 +1,13 @@
+package BeeCrowd;
+
+import java.util.Scanner;
+//1016
+public class DistanceCar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int minuteValue = scanner.nextInt();
+
+        System.out.println(2*minuteValue+" minutos");
+        scanner.close();
+    }
+}
